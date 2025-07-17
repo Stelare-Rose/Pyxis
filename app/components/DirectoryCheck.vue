05ac8a96652f86b143302a2b5a3823b3b594a066
Stelare-Rose@Snowflake <StelareRosaine+Github@gmail.com>
2025-07-17 15:36:50 +0700

    Added a check for the Pyxis Directory to make sure it exists

---
+++ app/components/DirectoryCheck.vue
@@ -0,0 +1,7 @@
+<script setup lang=ts>
+	import { exists, mkdir, BaseDirectory } from '@tauri-apps/plugin-fs';
+	const directoryExists = await exists('Pyxis', { baseDir: BaseDirectory.Data });
+	if(!directoryExists){
+		await mkdir('Pyxis', { baseDir: BaseDirectory.Data });
+	}
+</script>
