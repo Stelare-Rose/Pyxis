3f7fd9df757b4ebee7879ec284d7925fa08231e4
Stelare-Rose@Snowflake <StelareRosaine+Github@gmail.com>
2025-11-28 12:06:50 +0700

    Refactor: Moved Component Locations

---
+++ app/components/Bottombar/NewTask.vue
@@ -0,0 +1,34 @@
+<script setup lang=ts>
+	const enabled = ref(true);
+	const hovered = ref(false);
+	const color = useColors();
+	const clicked = () => {
+		console.log("Hello");
+		if(enabled.value){
+			ShowTaskModal();
+		}
+	}
+	onMounted(() => {
+		useTaskModalBus('active', e => enabled.value = !e);
+	});
+	onBeforeUnmount(() => {
+		RemoveTaskModalBus('active');
+	});
+</script>
+<template>
+	<Icon :height='24' :width='24' @click="clicked" @mouseenter="hovered = true" @mouseleave="hovered = false">
+		<ClipboardText 
+			:strokeColor="hovered ? color.text.orange : '#000'"
+		></ClipboardText>
+	</Icon>
+</template>
+<style scoped>
+	.newtask-button-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		z-index: 1;
+		box-sizing: border-box;
+		border: 2px solid #00000000;
+	}
+</style>
